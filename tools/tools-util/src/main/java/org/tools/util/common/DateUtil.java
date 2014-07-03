4c9e81eef3586cd797f2642ea5aa6f061b340b3c
李贺 <lihe@qianmi.com>
2014-07-03 14:51:32 +0800

    时间util添加注释

--- tools/tools-util/src/main/java/org/tools/util/common/DateUtil.java
+++ tools/tools-util/src/main/java/org/tools/util/common/DateUtil.java
@@ -13,16 +13,33 @@ public class DateUtil {
 
     public static final String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
+    /**
+     * 获取当前详细时间
+     * @return
+     */
     public static String getCurrentDay() {
         SimpleDateFormat sdf = new SimpleDateFormat(DATE_FORMAT);
         return sdf.format(new Date());
     }
 
+    /**
+     * 按照指定格式格式化时间
+     * @param date
+     * @param format
+     * @return
+     */
     public static String getDateString(Date date, String format) {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
         return sdf.format(date);
     }
 
+    /**
+     * 按照指定格式转换字符串to时间
+     * @param date
+     * @param format
+     * @return
+     * @throws ParseException
+     */
     public static Date getDate(String date, String format) throws ParseException {
         SimpleDateFormat sdf = new SimpleDateFormat(format);
         return sdf.parse(date);
